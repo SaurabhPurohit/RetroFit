a9d769d91c92f027d9fd79efdd7737dc1113c6bb
Saurabh Purohit <saurabh.letsnurture@gmail.com>
2017-02-06 19:03:51 +0530

    values add

--- app/src/main/java/com/example/ln_20/retrofit/adapter/MyAdapter.java
+++ app/src/main/java/com/example/ln_20/retrofit/adapter/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.example.ln_20.retrofit.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,10 @@ import android.widget.TextView;
 import com.example.ln_20.retrofit.R;
 import com.example.ln_20.retrofit.model.ApiResponse;
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by ln-20 on 6/2/17.
@@ -19,6 +24,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     String json;
     int i;
     ApiResponse apiResponse;
+    StringBuilder builder;
+    List<String> value;
 
     public MyAdapter(String json) {
         this.json = json;
@@ -27,7 +34,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.my_cards,parent,false);
+                .inflate(R.layout.my_cards, parent, false);
         return new MyViewHolder(view);
     }
 
@@ -36,38 +43,62 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
 
         holder.longName.setText(apiResponse.getResults()
-                .get(1)
+                .get(0)
                 .getAddressComponents()
                 .get(position)
                 .getLongName());
         holder.shortName.setText(apiResponse.getResults()
-                .get(1)
+                .get(0)
                 .getAddressComponents()
                 .get(position)
                 .getShortName());
+
+        value = new LinkedList<>();
+
+
+        value = apiResponse.getResults()
+                .get(0)
+                .getAddressComponents()
+                .get(position)
+                .getTypes();
+
+        Log.d("SIZE", String.valueOf(value.size()));
+
+        for (int i = 0; i < value.size(); i++) {
+            builder.append(apiResponse.getResults()
+                    .get(0)
+                    .getAddressComponents()
+                    .get(i)
+                    .getTypes());
+        }
+
+        holder.valueText.setText(builder);
+
+        builder.delete(0, builder.length());
+
+
     }
 
     @Override
     public int getItemCount() {
 
-//        return apiResponse.getResults().get(0).getAddressComponents().size();
-        apiResponse = new Gson().fromJson(json,ApiResponse.class);
-        return apiResponse.getResults().get(1).getAddressComponents().size();
+        apiResponse = new Gson().fromJson(json, ApiResponse.class);
+        builder = new StringBuilder();
+        apiResponse = new Gson().fromJson(json, ApiResponse.class);
+        return apiResponse.getResults().get(0).getAddressComponents().size();
     }
 
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
-        TextView longName , shortName;
+        TextView longName, shortName, valueText;
 
         public MyViewHolder(View itemView) {
             super(itemView);
 
             longName = (TextView) itemView.findViewById(R.id.longName);
             shortName = (TextView) itemView.findViewById(R.id.shortName);
+            valueText = (TextView) itemView.findViewById(R.id.value);
         }
     }
-
-
-
 }
